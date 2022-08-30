b9a6ad324b66b301084ae3bca4e5cc45f1671b1b
zacmaes <zachary.maes@gmail.com>
2022-08-30 11:09:04 -0700

    Initial Commit: added golden_hour.py with initial mostly blank comment header...lets get this project going

---
+++ golden_hour.py
@@ -0,0 +1,27 @@
+# Golden Hour App V1
+
+# Description:
+# ...
+#    ...
+#       ...
+
+
+# API DATA NEEDED
+# Pull data from a weather api
+# pull data from a sun position api
+# pull data from a time api
+# pull data from a location api
+
+
+# FEATURES
+
+#  ...
+#
+#
+#
+#
+#
+#
+#
+#
+#
