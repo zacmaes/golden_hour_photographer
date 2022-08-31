a061b8001889e066edcc9ca915c1c608bf7b3c3b
zacmaes <zachary.maes@gmail.com>
2022-08-31 09:21:08 -0700

    Commit 2c: added blueprints for views.py and auth.py, added test html h1 for home() func in home route

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)  # runs the web server, debug=True reruns the server after changes
-
+    app.run(host="localhost", port=8000, debug=True)  # runs the web server, debug=True reruns the server after changes
+        # host="localhost", port=8000... from
+        #   https://stackoverflow.com/questions/41940663/how-can-i-change-the-host-and-port-that-the-flask-command-uses
 
 
 
